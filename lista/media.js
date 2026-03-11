0eb1f4d6ae54e5c2926f692e3f09bffcfa4795b9
Erick-Machado022 <erickmachado050@gmail.com>
2026-03-11 19:15:22 -0300

    first commit

---
+++ lista/media.js
@@ -0,0 +1,11 @@
+let lisa = [1, 2, 3, 5, 4]
+let soma = 0
+
+for (let index = 0; index < lisa.length; index++) {
+    soma = soma + lisa[index]
+    
+}
+
+let media = soma / lisa.length
+
+console.log(media)
---
+++ lista/menorDe2.js
@@ -0,0 +1,11 @@
+let cont = 0
+
+let menor = 5
+let maior = 10
+
+if(menor < maior){
+    console.log(menor)
+}else{
+    console.log(maior)
+}
+
